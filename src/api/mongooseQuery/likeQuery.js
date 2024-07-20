fbdb47b966a05092c885ef80aec41c0583c93a76
huydayne168 <huynguyenviet1608@gmail.com>
2024-07-20 11:32:13 +0700

    check if like exist

--- src/api/mongooseQuery/likeQuery.js
+++ src/api/mongooseQuery/likeQuery.js
@@ -9,3 +9,8 @@ exports.createLikePost = (currentUserId, postId) => {
 exports.deleteLikePost = (currentUserId, postId) => {
     return Like.findOneAndDelete({ userId: currentUserId, postId });
 };
+
+// Find Like:
+exports.findLikePost = (currentUserId, postId) => {
+    return Like.findOne({ userId: currentUserId, postId });
+};
